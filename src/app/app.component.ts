db3ba6ea4691c275329095333d7d8bf566906117
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-11-05 14:37:27 +0200

    task-3 with ngClass and ngStyle durectives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayPassword = true;
+  counter = 0;
+  clicks = [];
   
+  onDisplayChange() {
+    this.displayPassword = !this.displayPassword;
+    this.clicks.push(this.counter++);
+    console.log(this.clicks);
+    
+  }
 }
