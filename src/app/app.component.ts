94d05c618043b9c101266199b8296f2d29d05538
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-11-15 20:11:05 +0200

    task-6 with template-driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: true}) form: NgForm;
+
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  defaultSubscription = 'Advanced';
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
 }
