804ecc20a705a99b4d86e3c01ded1d1df63f19e1
Oksana Rudkevych <oksana.rudkevych@gmail.com>
2019-11-16 17:44:42 +0200

    task-7 with reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Observable} from "rxjs";
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
